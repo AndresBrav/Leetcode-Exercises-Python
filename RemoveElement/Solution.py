de193be4a6db88ee2c933c2dbac2b5e1fdf67b12
Andres Bravo <bravoandres1706@gmail.com>
2025-12-11 08:28:23 -0400

    solution remove Element

--- RemoveElement/Solution.py
+++ RemoveElement/Solution.py
@@ -7,20 +7,18 @@ class Solution:
     """
 
     def removeElement(self, nums: List[int], val: int) -> int:
-
         i = 0
         while i < len(nums):
             if nums[i] == val:
-                nums.pop(i)  # remove this index
+                nums.pop(i)
             else:
-                i += 1       # we just advanced if we did not pop
-
+                i = i+1
         print(len(nums))
         return len(nums)
 
 
 sol = Solution()
-nums = [0, 1, 2, 2, 3, 0, 4, 2]
-val = 2
+nums = [3, 2, 2, 3]
+val = 3
 sol.removeElement(nums, val)
 print(nums)
