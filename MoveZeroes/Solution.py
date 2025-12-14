309a28493958af37a0ac3f497b2bc58a1a9b6fc0
Andres Bravo <bravoandres1706@gmail.com>
2025-12-14 09:26:53 -0400

    fist version delete zeros

--- MoveZeroes/Solution.py
+++ MoveZeroes/Solution.py
@@ -1,17 +1,24 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        print(nums)
+        # print(nums)
         """
         Do not return anything, modify nums in-place instead.
         """
-        size = len(nums)
-        i=0
         iterator= True
 
-        while i < len(nums):
-            print("size of array is "+str(len(nums)))
+        zeros = nums.count(0)
+
+        counter = 0
+        i=0
+        while iterator:
+            # print("size of array is "+str(len(nums)))
             if(nums[i]==0):
                 nums.pop(i)
+                counter +=1
+                if counter ==zeros:
+                    iterator = False
+                    for x in range(zeros):
+                        nums.append(0)
                 
             else:
                 i+=1
@@ -20,4 +27,5 @@ class Solution:
 
 
 a = Solution()
+# a.moveZeroes([0])
 a.moveZeroes([0,1,0,3,12])
